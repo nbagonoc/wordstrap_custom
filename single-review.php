8925f0d3ea62182fc16a8aa8c8f7bd0faa0623ef
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-04-03 21:49:45 +0800

    change single.php and single-review/custom_posttype_name.php

--- single-review.php
+++ single-review.php
@@ -1,32 +1,44 @@
 <?php get_header(); ?>
-	<div class="space-lg">
+	<div class="space-sm">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8">
 					<div class="content">
-						<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-							<h2 class="text-uppercase"><?php the_title(); ?></h2>
-							<p><?php echo get_the_content(); ?></p>
-                            <?php
-                                $nb_option = get_post_meta($post->ID, 'nb_option_meta_key', true);
-                                if(!empty($nb_option)){
-                                    echo 'meta box value: '. $nb_option;
-                                }
-							?>
-                        <?php echo get_the_term_list( $post->ID, 'product-type', '<ul class="list-inline"><li>The Mood:</li><li>', ',</li><li>', '</li></ul>' ); ?>
-                        <?php echo get_the_term_list( $post->ID, 'price', '<ul class="list-inline"><li>Price Range:</li><li>', ',</li><li>', '</li></ul>' ); ?>
-                        <?php echo get_the_term_list( $post->ID, 'mood', '<ul class="list-inline"><li>The Mood:</li><li>', ',</li><li>', '</li></ul>' ); ?>
-						<?php endwhile; ?>
-						<?php else: ?>
-						<?php endif; ?>
-						<?php wp_reset_postdata(); ?>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+								<h2 class="text-uppercase"><?php the_title(); ?></h2>
+								<p><?php echo get_the_content(); ?></p>
+								<!-- output nb_option_meta_box -->
+								<?php
+									$nb_option_meta_box = get_post_meta($post->ID, '_nb_option_meta_box_key', true);
+									if(!empty($nb_option_meta_box)){
+										echo 'meta box value: '. $nb_option_meta_box . '<br/>';
+									}
+								?>
+								<!-- output nb_field_meta_box -->
+								<?php
+									$nb_field_meta_box = get_post_meta($post->ID, '_nb_field_meta_box_key', true);
+									if(!empty($nb_field_meta_box)){
+										echo 'meta box value: '. $nb_field_meta_box;
+									}
+								?>
+								<?php echo get_the_term_list( $post->ID, 'product-type', '<ul class="list-inline"><li>The Mood:</li><li>', ',</li><li>', '</li></ul>' ); ?>
+								<?php echo get_the_term_list( $post->ID, 'price', '<ul class="list-inline"><li>Price Range:</li><li>', ',</li><li>', '</li></ul>' ); ?>
+								<?php echo get_the_term_list( $post->ID, 'mood', '<ul class="list-inline"><li>The Mood:</li><li>', ',</li><li>', '</li></ul>' ); ?>
+								<?php endwhile; ?>
+								<?php else: ?>
+								<?php endif; ?>
+								<?php wp_reset_postdata(); ?>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-4">
-					<div class="sidebar widget-area">
-						<?php get_sidebar('sidebar'); ?>
+				<div class="col-md-4 mobile-space">
+					<div class="sidebar" data-offset-smart-dynamic>
+						<?php dynamic_sidebar('sidebar'); ?>
 					</div>
-				</div>
+				</div><!-- /sidebar -->
 			</div>
 		</div>
 	</div>
--- single.php
+++ single.php
@@ -1,27 +1,42 @@
 <?php get_header(); ?>
-	<div class="space-lg">
+	<div class="space-md">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-8">
 					<div class="content">
-						<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-							<h2 class="text-uppercase"><?php the_title(); ?></h2>
-							<p><?php echo get_the_content(); ?></p>
-							<?php
-								$nb_option = get_post_meta($post->ID, 'nb_option_meta_key', true);
-								echo 'meta box value: '. $nb_option;
-							?>
-						<?php endwhile; ?>
-						<?php else: ?>
-						<?php endif; ?>
-						<?php wp_reset_postdata(); ?>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+								<h2 class="text-uppercase"><?php the_title(); ?></h2>
+								<p><?php echo get_the_content(); ?></p>
+								<!-- output nb_option_meta_box -->
+								<?php
+									$nb_option_meta_box = get_post_meta($post->ID, '_nb_option_meta_box_key', true);
+									if(!empty($nb_option_meta_box)){
+										echo 'meta box value: '. $nb_option_meta_box;
+									}
+								?>
+								<!-- output nb_field_meta_box -->
+								<?php
+									$nb_field_meta_box = get_post_meta($post->ID, '_nb_field_meta_box_key', true);
+									if(!empty($nb_field_meta_box)){
+										echo 'meta box value: '. $nb_field_meta_box;
+									}
+								?>
+								<?php endwhile; ?>
+								<?php else: ?>
+								<?php endif; ?>
+								<?php wp_reset_postdata(); ?>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-4">
-					<div class="sidebar widget-area">
-						<?php get_sidebar('sidebar'); ?>
+				<!-- sidebar -->
+				<div class="col-md-4 mobile-space">
+					<div class="sidebar" data-offset-smart-dynamic>
+						<?php dynamic_sidebar('sidebar'); ?>
 					</div>
-				</div>
+				</div><!-- /sidebar -->
 			</div>
 		</div>
 	</div>
